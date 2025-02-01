8fe32df8469c2534529ffb38f660b6a7e166f0c2
Nelson19so <nelsonsomto10@gmail.com>
2025-02-01 23:44:00 +0100

    Nelson19so

--- asset/javascript/form.js
+++ asset/javascript/form.js
@@ -1,17 +1,41 @@
 const avatarInput = document.getElementById("avatar");
 const displayImg = document.getElementById("img-user");
+const imgDescr = document.getElementById("descr");
+const displayBtnChanges = document.getElementById("display-btn");
+const removeImg = document.getElementById("Remove-img");
+const imageUpload = document.getElementById("image-upload");
+
+displayBtnChanges.style.display = "none";
 
 avatarInput.addEventListener("change", (event) => {
-  const selectfile = event.target.file[0];
-  const reader = new FileReader();
-  reader.addEventListener("load", (e) => {
-    displayImg.src = e.target.result;
-  });
+  if (event.target.files && event.target.files.length > 0) {
+    const selectfile = event.target.files[0];
+    const reader = new FileReader();
 
-  reader.readAsDataURL(selectfile);
+    reader.addEventListener("load", (e) => {
+      displayImg.src = e.target.result;
+    });
+
+    reader.readAsDataURL(selectfile);
+    avatarInput.style.display = "none";
+    imgDescr.style.display = "none";
+    displayBtnChanges.style.display = "flex";
+    displayBtnChanges.style.justifyContent = "center";
+  }
+});
+
+imageUpload.addEventListener("click", () => {
+  avatarInput.click();
+});
+
+removeImg.addEventListener("click", () => {
+  displayImg.src = "./asset/images/icon-upload.svg";
+  displayBtnChanges.style.display = "none";
+  imgDescr.style.display = "block";
 });
 
 // form validation.
+
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const github = document.getElementById("github");
@@ -37,16 +61,23 @@ submitForm.addEventListener("submit", (e) => {
   if (usernameValue == "" || usernameValue.length <= minLengh) {
     usernameError.classList.add("username-error");
     usernameError.style.display = "block";
+    username.style.border = "1px solid hsl(7, 88%, 67%)";
   } else if (emailValue == "") {
     emailError.classList.add("email-error");
     emailError.style.display = "block";
+    email.style.border = "1px solid hsl(7, 88%, 67%)";
   } else if (githubValue === "") {
     githubError.classList.add("github-error");
     githubError.style.display = "block";
+    github.style.border = "1px solid hsl(7, 88%, 67%)";
   } else {
+    username.style.border = "";
+    email.style.border = "";
+    github.style.border = "";
     usernameError.style.display = "none";
     emailError.style.display = "none";
     githubError.style.display = "none";
   }
 });
+
 // ---------------------------------
